b69464127c0d57edbc03ce83ccd23b1baf4c8de4
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-07 15:41:18 +0200

    JJ

--- src/helper/query-param-tool.spec.ts
+++ src/helper/query-param-tool.spec.ts
@@ -1,14 +1,38 @@
-import * as express from "express";
+import {
+    Request,
+    Response,
+    RequestHandler
+} from "express";
 import { expect } from 'chai';
+import * as sinon from 'sinon';
 import 'mocha';
 import { QueryParameterType, QueryParameterFilter, HandleQueryParameterFilter, ParseFromToQueryParameter } from './query-param-tools';
 
-describe('Hello function', () => {
+describe('asdf', () => {
 
+    var mockRequest;
+    var mockResponse;
+    var mockNext: sinon.spy;
+    beforeEach(() => {
+        mockRequest = sinon.mock({});
+        mockResponse = sinon.mock({});
+        mockNext = sinon.spy();
+        //mockRequest.expects("method").once().throws();
+    });
+
+    afterEach(() => {
+
+        //mockRequest.verify();
+        //mockResponse.verify();
+        sinon.assert.calledOnce(mockNext);
+    });
     it('should return hello world', () => {
         expect(1).to.equal(1);
-        let asdf: express.RequestHandler = ParseFromToQueryParameter();
+        let asdf: RequestHandler = ParseFromToQueryParameter();
         expect(asdf).to.be.not.null;
+        mockRequest = {};
+        mockResponse = {};
+        asdf(mockRequest, mockResponse, mockNext);
     });
 
 });
