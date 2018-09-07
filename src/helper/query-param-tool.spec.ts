78e55f8a7b65812078d89996c684370c28ff249f
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-07 16:29:04 +0200

    additional tests

--- src/helper/query-param-tool.spec.ts
+++ src/helper/query-param-tool.spec.ts
@@ -8,31 +8,59 @@ import * as sinon from 'sinon';
 import 'mocha';
 import { QueryParameterType, QueryParameterFilter, HandleQueryParameterFilter, ParseFromToQueryParameter } from './query-param-tools';
 
-describe('asdf', () => {
+describe('QueryParamTool', () => {
 
-    var mockRequest;
-    var mockResponse;
-    var mockNext: sinon.spy;
-    beforeEach(() => {
-        mockRequest = sinon.mock({});
-        mockResponse = sinon.mock({});
-        mockNext = sinon.spy();
-        //mockRequest.expects("method").once().throws();
-    });
+    describe("ParseFromToQUeryParameter", () => {
 
-    afterEach(() => {
+        let mockRequest;
+        let mockResponse;
+        let mockNext: sinon.spy;
+        beforeEach((done) => {
+            mockRequest = sinon.mock({});
+            mockResponse = sinon.mock({});
+            mockNext = sinon.spy();
+            done();
+            //mockRequest.expects("method").once().throws();
+        });
 
-        //mockRequest.verify();
-        //mockResponse.verify();
-        sinon.assert.calledOnce(mockNext);
-    });
-    it('should return hello world', () => {
-        expect(1).to.equal(1);
-        let asdf: RequestHandler = ParseFromToQueryParameter();
-        expect(asdf).to.be.not.null;
-        mockRequest = {};
-        mockResponse = {};
-        asdf(mockRequest, mockResponse, mockNext);
+        afterEach(() => {
+
+            //mockRequest.verify();
+            //mockResponse.verify();
+        });
+        it('should pass without query arguments', () => {
+            expect(1).to.equal(1);
+            let asdf: RequestHandler = ParseFromToQueryParameter();
+            expect(asdf).to.be.not.null;
+            mockRequest = {};
+            mockResponse = {};
+            asdf(mockRequest, mockResponse, mockNext);
+            sinon.assert.calledOnce(mockNext);
+            expect(mockNext.getCall(0).args.length).to.be.equal(0);
+            expect(mockRequest).to.deep.equal({});
+        });
+        it('should pass with query arguments', () => {
+            expect(1).to.equal(1);
+            let asdf: RequestHandler = ParseFromToQueryParameter();
+            expect(asdf).to.be.not.null;
+            mockRequest = { query: {} };
+            mockResponse = {};
+            asdf(mockRequest, mockResponse, mockNext);
+            sinon.assert.calledOnce(mockNext);
+            expect(mockNext.getCall(0).args.length).to.be.equal(0);
+            expect(mockRequest).to.deep.equal({ query: {} });
+        });
+        it('should pass with query arguments for from', () => {
+            expect(1).to.equal(1);
+            let asdf: RequestHandler = ParseFromToQueryParameter();
+            expect(asdf).to.be.not.null;
+            mockRequest = { query: { from: "19" } };
+            mockResponse = {};
+            asdf(mockRequest, mockResponse, mockNext);
+            sinon.assert.calledOnce(mockNext);
+            expect(mockNext.getCall(0).args.length).to.be.equal(0);
+            expect(mockRequest).to.deep.equal({ query: { from: 19 } });
+        });
     });
 
 });
