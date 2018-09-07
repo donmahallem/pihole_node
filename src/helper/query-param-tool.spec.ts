44c3b8c6f0c71bc99cd4617ef05cb039a1d94f39
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-07 11:38:08 +0200

    test setup

---
+++ src/helper/query-param-tool.spec.ts
@@ -0,0 +1,11 @@
+
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello function', () => {
+
+    it('should return hello world', () => {
+        expect(1).to.equal(1);
+    });
+
+});
