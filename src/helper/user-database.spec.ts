2fb4767dc92c545b56a74671332de009d9ba943e
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-13 12:14:49 +0200

    user database test for bcrypt

---
+++ src/helper/user-database.spec.ts
@@ -0,0 +1,62 @@
+import {
+    Request,
+    Response,
+    RequestHandler
+} from "express";
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import 'mocha';
+import * as testObject from './user-database';
+import {
+    RouteError
+} from "../routes/route-error";
+import * as bcrypt from "bcrypt";
+
+describe('src/helper/user-database', () => {
+
+    describe("setPassword", () => {
+
+        var mockBcrypt: sinon.SinonStub;
+        beforeEach(() => {
+            mockBcrypt = sinon.stub(bcrypt, "hash");
+        });
+
+        afterEach(() => {
+            mockBcrypt.restore();
+        });
+        it('should pass without query arguments', (done) => {
+            mockBcrypt.returns(new Promise<string>((resolve, reject) => {
+                resolve("success");
+            }));
+            let asdf: testObject.UserDatabase = new testObject.UserDatabase();
+            let resultSpy: sinon.SinonSpy = sinon.spy();
+            let testResult = (err?: Error) => {
+                expect(err).to.not.exist;
+                expect(mockBcrypt.callCount).to.equal(1);
+                expect(mockBcrypt.getCall(0).args).to.deep.equal(["asdfaf", 12]);
+                expect(resultSpy.callCount).to.equal(1);
+                expect(resultSpy.getCall(0).args).to.deep.equal(["success"]);
+                done();
+            };
+            asdf.createHashedPassword("asdfaf")
+                .subscribe(resultSpy, testResult, testResult);
+        });
+        it('should fail pass without query arguments', (done) => {
+            mockBcrypt.returns(new Promise<string>((resolve, reject) => {
+                reject(new Error("random problem"));
+            }));
+            let asdf: testObject.UserDatabase = new testObject.UserDatabase();
+            let resultSpy: sinon.SinonSpy = sinon.spy();
+            let testResult = (err?: Error) => {
+                expect(err).to.exist;
+                expect(err.message).to.equal("random problem");
+                expect(mockBcrypt.callCount).to.equal(1);
+                expect(mockBcrypt.getCall(0).args).to.deep.equal(["asdfaf", 12]);
+                expect(resultSpy.callCount).to.equal(0);
+                done();
+            };
+            asdf.createHashedPassword("asdfaf")
+                .subscribe(resultSpy, testResult, testResult);
+        });
+    });
+});
---
+++ src/helper/user-database.ts
@@ -0,0 +1,130 @@
+import { Database, Statement } from 'sqlite3';
+import { Observable, Observer, of, merge } from "rxjs";
+import {
+    mergeMap,
+    observeOn,
+    subscribeOn,
+    map
+} from "rxjs/operators";
+import * as bcrypt from "bcrypt";
+
+export interface Query {
+    //autoincrement ID for the table, only used by SQLite3, not by FTLDNS
+    id: number;
+    //Unix timestamp when this query arrived at FTLDNS (used as index)
+    timestamp: number;
+    //Type of this query (see Supported query types)
+    type: number;
+    //How was this query handled by FTLDNS? (see Supported status types)
+    status: number;
+    //Requested domain
+    domain: string;
+    //Requesting client (IP address)
+    client: string;
+    //Forward destination used for this query (only set if status == 2)
+    forward?: string | undefined;
+}
+
+function prepareStatement(db: Database, statement: string, params?: any) {
+    return Observable.create((pub: Observer<Statement>) => {
+        db.serialize(() => {
+            const stat: Statement = db.prepare(statement, params);
+            pub.next(stat);
+            pub.complete();
+        });
+    });
+}
+function statementToList(stat: Statement): Observable<any> {
+    return Observable.create((pub: Observer<any>) => {
+        stat.each((err: Error, row: any) => {
+            if (err) {
+            } else {
+                pub.next(row);
+            }
+        }, (err: Error, count: number) => {
+            if (err) {
+                pub.error(err);
+            } else {
+                pub.complete();
+            }
+            stat.finalize();
+        });
+    });
+}
+
+
+
+
+export class UserDatabase {
+
+    private database: Database;
+    constructor() {
+        this.database = new Database("user.db");
+    }
+
+    public createHashedPassword(password: string, saltOrRounds: string | number = 12): Observable<string> {
+        return merge(bcrypt.hash(password, saltOrRounds));
+    }
+
+    public setPassword(user: string, password: string): Observable<string> {
+        return merge(bcrypt.hash(password, 12))
+            .pipe(map((val) => {
+                return val;
+            }));
+    }
+
+    public getQueries(limit: number = 25, offset: number = 0, client: string = undefined): Observable<Query> {
+        let paramLimit: number = 25;
+        let paramOffset: number = 0;
+        let paramClient: string = undefined;
+        if (limit && Number.isInteger(limit) && limit >= 0) {
+            paramLimit = limit;
+        }
+        if (offset && Number.isInteger(offset) && offset >= 0) {
+            paramOffset = offset;
+        }
+        if (client) {
+            paramClient = client;
+        }
+        if (paramClient) {
+            return prepareStatement(this.database, "SELECT * FROM queries WHERE client == ? ORDER BY timestamp DESC LIMIT ? OFFSET ?", [client, limit, offset])
+                .pipe(mergeMap((stat: Statement) => {
+                    return statementToList(stat);
+                }));
+        } else {
+            return prepareStatement(this.database, "SELECT * FROM queries ORDER BY timestamp DESC LIMIT ? OFFSET ?", [limit, offset])
+                .pipe(mergeMap((stat: Statement) => {
+                    return statementToList(stat);
+                }));
+        }
+    }
+
+    public getTopClients(limit: number = 25, offset: number = 0) {
+        return prepareStatement(this.database, "SELECT client, count(client) as num FROM queries GROUP by client order by count(client) desc limit ? OFFSET ?", [limit, offset])
+            .pipe(mergeMap((stat: Statement) => {
+                return statementToList(stat);
+            }));
+
+    }
+    public getTopDomains(limit: number = 25, offset: number = 0, client?: string): Observable<any> {
+        if (client) {
+            return prepareStatement(this.database, "SELECT domain,count(domain) as num FROM queries WHERE (client == ?) GROUP by domain order by count(domain) desc limit ? OFFSET ?", [client, limit, offset])
+                .pipe(mergeMap((stat: Statement) => {
+                    return statementToList(stat);
+                }));
+        } else {
+            return prepareStatement(this.database, "SELECT domain,count(domain) as num FROM queries GROUP by domain order by count(domain) desc limit ? OFFSET ?", [limit, offset])
+                .pipe(mergeMap((stat: Statement) => {
+                    return statementToList(stat);
+                }));
+        }
+
+    }
+    public getTopAds(limit: number = 25, offset: number = 0): Observable<any> {
+        return prepareStatement(this.database, "SELECT domain,count(domain) as num FROM queries WHERE (STATUS == 1 OR STATUS == 4) GROUP by domain order by count(domain) desc limit ? OFFSET ?", [limit, offset])
+            .pipe(mergeMap((stat: Statement) => {
+                return statementToList(stat);
+            }));
+
+    }
+}
