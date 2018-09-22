0fef1b5b8227b395065e6cf2afee3ece0416b0ef
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-22 14:49:39 +0200

    fix smell

--- src/middleware/permission.middleware.ts
+++ src/middleware/permission.middleware.ts
@@ -1,30 +1,30 @@
-/// <reference path="../extensions.d.ts" />
-import * as express from "express";
-import { RouteError } from "../routes/route-error";
-import * as jwt from "jsonwebtoken";
-import { UserPermission } from "../helper/user-permissions";
+/// <reference path='../extensions.d.ts' />
+import * as express from 'express';
+import { RouteError } from '../routes/route-error';
+import * as jwt from 'jsonwebtoken';
+import { UserPermission } from '../helper/user-permissions';
 
 export const createUnauthorizedUser = () => {
     return {
         authorized: false
-    }
-}
+    };
+};
 
 export const createAuthorizationMiddleware = (): express.RequestHandler => {
     return (req: express.Request, res: express.Response, next: express.NextFunction) => {
         if (req.headers && req.headers.authorization) {
             if (req.user) {
-                next(new RouteError(500, "User Object already set"));
+                next(new RouteError(500, 'User Object already set'));
                 return;
             }
             const options: jwt.VerifyOptions = {
 
             };
-            jwt.verify(req.headers.authorization, "key", options, (err: jwt.VerifyErrors, decoded: object | string) => {
+            jwt.verify(req.headers.authorization, 'key', options, (err: jwt.VerifyErrors, decoded: object | string) => {
                 if (err) {
                     req.user = createUnauthorizedUser();
                 } else if (decoded) {
-                    let anyData: any = decoded;
+                    const anyData: any = decoded;
                     if (anyData.user)
                         req.user = anyData.user;
                 } else {
@@ -37,15 +37,14 @@ export const createAuthorizationMiddleware = (): express.RequestHandler => {
             next();
         }
     }
-}
-
+};
 
 export const createPermissionMiddleware = (permissions: UserPermission[]): express.RequestHandler => {
     return (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        if (permissions.length == 0) {
+        if (permissions.length === 0) {
             next();
         } else {
             next();
         }
     }
-}
+};
