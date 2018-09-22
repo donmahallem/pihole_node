8847d15fd05e01d3c87c15710b9946836b8b58a1
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-22 13:28:20 +0200

    add test for missing auhtorization header

--- src/middleware/permission.middleware.spec.ts
+++ src/middleware/permission.middleware.spec.ts
@@ -45,5 +45,36 @@ describe('src/middleware/permission.middleware', () => {
             expect(nextSpy.callCount).to.equal(1);
             expect(nextSpy.getCall(0).args.length).to.equal(0);
         });
+        it('should pass as unauthorized without header.authorization object', () => {
+            const testInstance: any = { testdata1: 129, testdata2: "asdf" };
+            verifyStub.callsArgWith(2, []);
+            let asdf: RequestHandler = testObject.createAuthorizationMiddleware();
+            let queryData: any = { offset: 2 };
+            let req: any = { query: queryData, headers: { notAuthorization: 299 } };
+            let res: any = {};
+            expect(asdf).to.be.not.null;
+            asdf(req, res, <any>nextSpy);
+            expect(req.user).to.exist;
+            expect(req.user).to.deep.equal({ "authorized": false });
+            expect(nextSpy.callCount).to.equal(1);
+            expect(nextSpy.getCall(0).args.length).to.equal(0);
+        });
+        it('should throw error for a user already being set', () => {
+            const testInstance: any = { testdata1: 129, testdata2: "asdf" };
+            verifyStub.callsArgWith(2, []);
+            let asdf: RequestHandler = testObject.createAuthorizationMiddleware();
+            let queryData: any = { offset: 2 };
+            let req: any = { query: queryData, user: {}, headers: { authorization: 299 } };
+            let res: any = {};
+            expect(asdf).to.be.not.null;
+            asdf(req, res, <any>nextSpy);
+            expect(req.user).to.exist;
+            expect(req.user).to.deep.equal({});
+            expect(nextSpy.callCount).to.equal(1);
+            expect(nextSpy.getCall(0).args.length).to.equal(1);
+            const err: RouteError = nextSpy.getCall(0).args[0];
+            expect(err.statusCode).to.equal(500);
+            expect(err.message).to.equal("User Object already set");
+        });
     });
 });
