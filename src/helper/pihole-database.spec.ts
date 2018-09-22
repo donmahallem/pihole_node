958d0608a86271bf78a9b05fd1794c7f99bac023
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-22 10:12:30 +0200

    added test for PiholeDatabase

--- src/helper/pihole-database.spec.ts
+++ src/helper/pihole-database.spec.ts
@@ -10,27 +10,64 @@ import * as testObject from './pihole-database';
 import {
     RouteError
 } from "../routes/route-error";
-
+import * as sqlite from "sqlite3";
+import { DatabaseUtil } from "./database-util";
+import { of } from 'rxjs';
 describe('src/helper/pihole-database', () => {
 
     describe("PiholeDatabase", () => {
 
-        var mockRequest;
-        var mockResponse;
         var mockNext: sinon.SinonSpy;
-        beforeEach((done) => {
-            mockRequest = sinon.mock({});
-            mockResponse = sinon.mock({});
+        var sqliteCacheStub: sinon.SinonStub;
+        var sqliteDbStub: sinon.SinonStubbedInstance<sqlite.Database>;
+        before(() => {
+            sqliteCacheStub = sinon.stub(sqlite.cached, "Database");
+            sqliteDbStub = sinon.createStubInstance(sqlite.Database);
+            sqliteCacheStub.returns(sqliteDbStub);
             mockNext = sinon.spy();
-            done();
-            //mockRequest.expects("method").once().throws();
         });
 
         afterEach(() => {
             //mockRequest.verify();
             //mockResponse.verify();
+            sqliteCacheStub.resetHistory();
+            mockNext.resetHistory();
         });
-        it('should pass without query arguments', () => {
+
+        after(() => {
+            sqliteCacheStub.restore();
+        })
+        describe("getTopClients", () => {
+            let databaseUtilStub: sinon.SinonStub;
+            beforeEach(() => {
+                databaseUtilStub = sinon.stub(DatabaseUtil, "listQuery");
+                databaseUtilStub.returns(of(1, 2, 3, 8, 9));
+            });
+            afterEach(() => {
+                databaseUtilStub.restore();
+            });
+            it('should pass without query arguments', (done) => {
+                const query: string = 'SELECT client, count(client) as num FROM queries'
+                    + ' GROUP by client order by count(client) desc limit ? OFFSET ?';
+                const limit: number = 200;
+                const offset: number = 20;
+                const queryParams: any[] = [limit, offset];
+                const db: testObject.PiholeDatabase = testObject.PiholeDatabase.getInstance();
+                db.getTopClients(limit, offset)
+                    .subscribe(mockNext, (err: Error) => {
+                        done(err);
+                    }, () => {
+                        expect(mockNext.callCount).to.equal(5);
+                        expect(mockNext.args).to.deep.equal([[1], [2], [3], [8], [9]]);
+                        expect(databaseUtilStub.callCount).to.equal(1);
+                        const args: any[] = databaseUtilStub.getCall(0).args;
+                        expect(args[0]).to.equal(sqliteDbStub);
+                        expect(args[1]).to.deep.equal(query);
+                        expect(args[2]).to.deep.equal(queryParams);
+                        //expect(databaseUtilStub.getCall(0).args).to.deep.equal([db, query, queryParams]);
+                        done();
+                    })
+            });
         });
     });
 });
