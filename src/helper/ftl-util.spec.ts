f28bb381eaa9a2a8772c2426dff86218b317103f
DonMahallem <donmahallem@users.noreply.github.com>
2018-09-24 20:40:50 +0200

    initnial test

---
+++ src/helper/ftl-util.spec.ts
@@ -0,0 +1,92 @@
+import {
+    Request,
+    Response,
+    RequestHandler
+} from "express";
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import 'mocha';
+import * as testObject from './ftl-util';
+import {
+    RouteError
+} from "../routes/route-error";
+import * as bcrypt from "bcrypt";
+import * as rxjsOperator from "rxjs/operators";
+import * as rxjs from 'rxjs';
+
+describe('src/helper/ftl-util', () => {
+
+    describe("FTLUtil", () => {
+
+        var ftlUtilInstance: testObject.FTLUtil;
+        beforeEach(() => {
+            ftlUtilInstance = new testObject.FTLUtil();
+        });
+
+        afterEach(() => {
+        });
+
+        describe("getStats", () => {
+
+            const testData: string[] = ["domains_being_blocked 130560",
+                "",
+                "dns_queries_today 2748",
+                "ads_blocked_today 753",
+                "ads_percentage_today 27.401747",
+                "unique_domains 423",
+                "queries_forwarded 1792",
+                "queries_cached 203",
+                "clients_ever_seen 7",
+                "unique_clients 7",
+                "dns_queries_all_types 2771",
+                "reply_NODATA 19",
+                "reply_NXDOMAIN 9",
+                "reply_CNAME 190",
+                "reply_IP 429",
+                "status enabled",
+                "---EOM---"];
+            const testDataResult: any = {
+                "domains_being_blocked": 130560,
+                "dns_queries_today": 2748,
+                "ads_blocked_today": 753,
+                "ads_percentage_today": 27.401747,
+                "unique_domains": 423,
+                "queries_forwarded": 1792,
+                "queries_cached": 203,
+                "clients_ever_seen": 7,
+                "unique_clients": 7,
+                "dns_queries_all_types": 2771,
+                "reply_NODATA": 19,
+                "reply_NXDOMAIN": 9,
+                "reply_CNAME": 190,
+                "reply_IP": 429,
+                "status": "enabled"
+            };
+            var stubSendRequest: sinon.SinonStub;
+
+            beforeEach(() => {
+                stubSendRequest = sinon.stub(testObject.FTLUtil, "sendRequest");
+            });
+
+            afterEach(() => {
+                stubSendRequest.restore();
+            });
+            it('should pass and encrypt password with default parameter', (done) => {
+
+                stubSendRequest.callsFake(() => {
+                    return rxjs.from(testData);
+                });
+                testObject.FTLUtil.getStats()
+                    .subscribe((dat) => {
+                        expect(dat).to.deep.equal(testDataResult);
+                        console.log("aa", dat);
+                    }, (err) => {
+                        console.error(err);
+                        done(err);
+                    }, () => {
+                        done();
+                    })
+            });
+        });
+    });
+});
---
+++ src/helper/ftl-util.ts
@@ -0,0 +1,84 @@
+import { Socket } from 'net';
+import {
+    Observable,
+    Observer,
+    from
+} from 'rxjs';
+import {
+    merge,
+    reduce,
+    filter,
+    map
+} from 'rxjs/operators';
+
+export class FTLUtil {
+
+    public static sendRequest(req: string, host: string = 'localhost', port: number = 4711): Observable<any> {
+        return Observable.create((pub: Observer<any>) => {
+
+            var client = new Socket();
+            client.on('error', (err: Error) => {
+                if (pub.closed) {
+                    return;
+                }
+                pub.error(err);
+            });
+            client.on('data', (data: Buffer) => {
+                if (pub.closed) {
+                    return;
+                }
+                pub.next(data.toString("utf-8"));
+            });
+
+            client.on('close', () => {
+                if (pub.closed) {
+                    return;
+                }
+                pub.complete();
+            });
+            client.on('connect', () => {
+                //client.write(req);
+                client.end(req);
+            });
+            client.connect(port, host);
+
+        });
+    }
+
+    public static getStats(host: string = 'localhost', port: number = 4711): Observable<any> {
+        const intRegex: RegExp = /^[0-9]+$/;
+        const floatRegex: RegExp = /^[0-9]+\.[0-9]*$/;
+        return this.sendRequest('>stats', host, port)
+            .pipe(map((item: string) => {
+                console.log(item);
+                if (item !== undefined) {
+                    return item.trim();
+                }
+            }), filter((item: string): boolean => {
+                const a: RegExp = /^[a-zA-Z\_]+ ([0-9\.]+|enabled|disabled)$/;
+                return a.test(item);
+            }), map((item: string) => {
+                const parts: any = item.split(' ');
+                return parts;
+            }), filter((values: string[]) => {
+                return values.length === 2;
+            }), map((values: any[]) => {
+                if (intRegex.test(values[1])) {
+                    values[1] = parseInt(values[1]);
+                } else if (floatRegex.test(values[1])) {
+                    values[1] = parseFloat(values[1]);
+                }
+                return values;
+            }), reduce((acc: any, item: any[]) => {
+                if (Array.isArray(acc)) {
+                    const obj: any = {};
+                    obj[acc[0]] = acc[1];
+                    obj[item[0]] = item[1];
+                    return obj;
+                } else {
+                    acc[item[0]] = item[1];
+                    return acc;
+                }
+            }));
+    }
+}
