7ac18b1cdabed988f3e69f61a4930669fe3feb18
DonMahallem <donmahallem@users.noreply.github.com>
2018-10-05 00:31:48 +0200

    missing tests

--- src/routes/top/clients.endpoint.spec.ts
+++ src/routes/top/clients.endpoint.spec.ts
@@ -12,45 +12,129 @@ import {
 import * as express from "express";
 import * as testObject from "./clients.endpoint";
 import * as httpMocks from "node-mocks-http";
-import { PiholeDatabase } from "../../helper/pihole-database";
 import { of as rxjsOf } from "rxjs";
+import * as supertest from 'supertest';
+import * as commonUtil from './common-util';
+import * as jsonschema from 'jsonschema';
+import { sqlite3 } from "sqlite3";
+import { PiholeDatabase } from "../../helper/pihole-database";
 describe('routes/top/clients.endpoint', () => {
     describe('createTopClientsEndpoint', function () {
-        var nextSpy: sinon.SinonSpy;
-        var databaseStubbedInstance: sinon.SinonStubbedInstance<PiholeDatabase>;
+        let expressApp: express.Express;
+        let createSchemaValidatorStub: sinon.SinonStub;
+        let validatorStubInstance: sinon.SinonStubbedInstance<jsonschema.Validator>;
+        let databaseInstanceStub: sinon.SinonStubbedInstance<PiholeDatabase>;
         before(function () {
-            nextSpy = sinon.spy();
-            databaseStubbedInstance = sinon.createStubInstance(PiholeDatabase);
-            databaseStubbedInstance.getTopClients
-                .callsFake(() => {
-                    return rxjsOf({ "test": "object" });
-                })
+            createSchemaValidatorStub = sinon.stub(testObject, "createSchemaValidator");
+        });
+        beforeEach(() => {
+            expressApp = express();
+            validatorStubInstance = sinon.createStubInstance(jsonschema.Validator);
+            createSchemaValidatorStub.returns(validatorStubInstance);
+            databaseInstanceStub = sinon.createStubInstance(PiholeDatabase);
         });
         afterEach(() => {
-            databaseStubbedInstance.getTopClients.reset();
+            expect(validatorStubInstance.validate.callCount).to.equal(1);
+            expect(createSchemaValidatorStub.callCount).to.equal(1);
+            createSchemaValidatorStub.reset();
         });
         after(() => {
+            createSchemaValidatorStub.restore();
         })
+        describe('schema is invalid', () => {
+            let dbCallback: sinon.SinonStub;
+            beforeEach(() => {
+                dbCallback = sinon.stub();
+                validatorStubInstance.validate.returns({
+                    valid: false,
+                    errors: [{ message: "test error message" }]
+                });
+            });
+            afterEach(() => {
+                dbCallback.resetHistory();
+            });
+            after(() => {
 
-        it('should respond with correct data', (done) => {
-            let req = httpMocks.createRequest({
-                query: {
-                    limit: 25
-                }
             });
-            let res = httpMocks.createResponse({
-                eventEmitter: require('events').EventEmitter
+            it('should respond with client present', () => {
+                expressApp.all("/", testObject.createTopClientsEndpoint(<any>databaseInstanceStub));
+                return supertest(expressApp)
+                    .get("/")
+                    .expect(401)
+                    .then((data: supertest.Response) => {
+
+                    });
             });
-            res.on('end', function () {
-                expect(nextSpy.callCount).to.equal(0);
-                expect(res.statusCode).to.equal(200);
-                let respBody: any = JSON.parse(res._getData());
-                expect(res.header("Content-Type")).to.equal("application/json");
-                expect(respBody).to.deep.equal({ data: [{ "test": "object" }] });
-                done();
+        });
+        describe('schema is valid', () => {
+            beforeEach(() => {
+                validatorStubInstance.validate.returns({
+                    valid: true
+                });
+                databaseInstanceStub.getTopClients.returns(rxjsOf("a", "b", "d"));
             });
-            const testa = testObject.createTopClientsEndpoint(<any>databaseStubbedInstance);
-            testa(req, res, nextSpy);
+            afterEach(() => {
+                expect(databaseInstanceStub.getTopClients.callCount).to.equal(1);
+                databaseInstanceStub.getTopClients.reset();
+            });
+            after(() => {
+
+            });
+            it('should respond default parameter values', () => {
+                expressApp.all("/", testObject.createTopClientsEndpoint(<any>databaseInstanceStub));
+                return supertest(expressApp)
+                    .get("/")
+                    .expect(200)
+                    .then((data: supertest.Response) => {
+                        expect(data.body).to.deep.equal({
+                            data: ["a", "b", "d"]
+                        });
+                        expect(databaseInstanceStub.getTopClients.getCall(0).args).to.deep.equal([25, 0]);
+                        expect(validatorStubInstance.validate.getCall(0).args).to
+                            .deep.equal([{},
+                            testObject.ClientsEndpointSchema]);
+                    });
+            });
+            it('should respond with all parameters provided', () => {
+                expressApp.all("/", testObject.createTopClientsEndpoint(<any>databaseInstanceStub));
+                return supertest(expressApp)
+                    .get("/")
+                    .query({
+                        offset: 12,
+                        limit: 13
+                    })
+                    .expect(200)
+                    .then((data: supertest.Response) => {
+                        expect(data.body).to.deep.equal({
+                            data: ["a", "b", "d"]
+                        });
+                        expect(databaseInstanceStub.getTopClients.getCall(0).args).to.deep.equal([13, 12]);
+                        expect(validatorStubInstance.validate.getCall(0).args).to.deep.equal([{
+                            offset: "12",
+                            limit: "13"
+                        }, testObject.ClientsEndpointSchema]);
+                    });
+            });
+            it('should respond with all parameters provided except client', () => {
+                expressApp.all("/", testObject.createTopClientsEndpoint(<any>databaseInstanceStub));
+                return supertest(expressApp)
+                    .get("/")
+                    .query({
+                        offset: 12,
+                        limit: 13
+                    })
+                    .expect(200)
+                    .then((data: supertest.Response) => {
+                        expect(data.body).to.deep.equal({
+                            data: ["a", "b", "d"]
+                        });
+                        expect(databaseInstanceStub.getTopClients.getCall(0).args).to.deep.equal([13, 12]);
+                        expect(validatorStubInstance.validate.getCall(0).args).to.deep.equal([{
+                            offset: "12",
+                            limit: "13"
+                        }, testObject.ClientsEndpointSchema]);
+                    });
+            });
         });
 
     });
--- src/routes/top/clients.endpoint.ts
+++ src/routes/top/clients.endpoint.ts
@@ -3,19 +3,42 @@ import { RouteError } from '../route-error';
 import { PiholeDatabase } from '../../helper/pihole-database';
 import { ParseLimitQueryParameter } from '../../helper/query-param-tools';
 import { createListResponseObserver } from '../../response/list-response.observer';
+import { ValidatorResult, Validator, Schema } from 'jsonschema';
+import { PageLimitSchema, PageOffsetSchema } from '../../schemas';
 
+export const ClientsEndpointSchema: Schema = {
+    id: '/TopEndpointSchema',
+    type: 'object',
+    properties: {
+        offset: { $ref: PageOffsetSchema.id },
+        limit: { $ref: PageLimitSchema.id }
+    }
+};
+
+export const createSchemaValidator = (): Validator => {
+    const val: Validator = new Validator();
+    val.addSchema(PageLimitSchema, PageLimitSchema.id);
+    val.addSchema(PageOffsetSchema, PageOffsetSchema.id);
+    return val;
+};
 
 export const createTopClientsEndpoint = (database: PiholeDatabase): express.RequestHandler => {
+    const schemaValidator: Validator = createSchemaValidator();
     return (req: express.Request, res: express.Response, next: express.NextFunction): void => {
-        let queryLimit: number = 25;
-        let queryOffset: number = 0;
-        if (req.query.limit) {
-            queryLimit = parseInt(req.query.limit, 10);
+        const validatonResult: ValidatorResult = schemaValidator.validate(req.query, ClientsEndpointSchema);
+        if (validatonResult.valid === true) {
+            let queryLimit: number = 25;
+            let queryOffset: number = 0;
+            if (req.query.limit) {
+                queryLimit = parseInt(req.query.limit, 10);
+            }
+            if (req.query.offset) {
+                queryOffset = parseInt(req.query.offset, 10);
+            }
+            database.getTopClients(queryLimit, queryOffset)
+                .subscribe(createListResponseObserver(req, res, next));
+        } else {
+            next(RouteError.fromValidatorError(validatonResult.errors));
         }
-        if (req.query.offset) {
-            queryOffset = parseInt(req.query.offset, 10);
-        }
-        database.getTopClients(queryLimit, queryOffset)
-            .subscribe(createListResponseObserver(req, res, next));
     };
 };
